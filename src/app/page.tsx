51996368fb2cdf284871dbe66d56923616943123
felicio.angga <felicio.angga@s4homedan.local>
2024-10-21 10:24:42 +0700

    add Dashboard UI and logic, most liked discussion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import AuthWrapper from "./components/AuthWrapper";
+import Dashboard from "./dashboard/components/Dashboard";
 
 export default function Home() {
   return (
     <AuthWrapper>
       <main className="min-h-screen bg-[#1E252B]">
-        
+        <Dashboard />
       </main>
     </AuthWrapper>
   )
